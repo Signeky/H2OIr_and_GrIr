5c3d53c96372dd1ee23ca204ec7d701599faf172
slambrick <sam.m@lambrick.org.uk>
2023-05-22 11:55:59 +0100

    Plotting diffraction as a function of DeltaK.

--- figure_DiffractionGrIr.m
+++ figure_DiffractionGrIr.m
@@ -1,13 +1,13 @@
 
-clear all; clf
+clear all; %clf
 fileList = {'as010363','as010365'};
 names = {'GrIr(111)','H_2O GrIr(111)'};
 
 colourList = 'rbgcmk';
 
-figure=figure('units','centimeters','position',[4,2,16,14],'color','white','DefaultLineLineWidth',2,'DefaultTextFontSize',18);
+figure('units','centimeters','position',[4,2,16,14],'color','white','DefaultLineLineWidth',2,'DefaultTextFontSize',18);
 
-addpath Diffraction
+addpath('Diffraction', 'Calibration')
 
 for i=1:length(fileList)
     load(fileList{i})
@@ -29,4 +29,45 @@ legend
 grid
 hold off
 
+%% Plot with Delta K rather than angle
 
+% Use the calibration from 08/12/2021
+load('beam_20211208_1219.mat')
+E0 = beam.E0; % meV
+
+E0 = E0*1.603e-19/1e3; % J
+h = 6.626e-34; % Planck constant, Js
+m = 3.0160293*1.66054e-27; % Helium 3 rest mass, kg
+k = (2*pi*sqrt(2*E0*m)/h); % m^-1, incident wavector
+k = k/1e10; % A^-1
+disp(['k = ' num2str(k) ' A^-1'])
+
+load(fileList{1})
+IrGr_gamma = meas.scan.angles-meas.ga;
+IrGr_counts = meas.scan.counts;
+IrGr_DK = -2*k*cosd(22.2)*sind(IrGr_gamma);
+load(fileList{2})
+IrGrH20_gamma = meas.scan.angles-meas.ga;
+IrGrH20_counts = meas.scan.counts;
+IrGrH20_DK = -2*k*cosd(22.2)*sind(IrGrH20_gamma);
+
+figure('units','centimeters','position',[4,2,16,14],'color','white','DefaultLineLineWidth',2,'DefaultTextFontSize',18);
+semilogy(IrGr_DK, IrGr_counts, 'displayname', names{1})
+hold on
+semilogy(IrGrH20_DK, IrGrH20_counts, 'displayname', names{2})
+hold off
+xlabel('\Delta K (A^{-1})')
+ylabel('Scattered signal (arb units)')
+legend
+set(gca,'YScale','log','LineWidth',1.5,'FontSize',18); set(gca,'ticklength',1.5*get(gca,'ticklength'))
+grid on
+
+% Find the diffraction peak to measure the lattice constant
+ind = IrGr_DK < -2.8 & IrGr_DK > -3.16;
+y = flip(IrGr_counts(ind));
+x = flip(IrGr_DK(ind));
+pks = findpeaks(y, x);
+G = abs(x(y == pks));
+a = (2*pi/G)/cosd(30); % Accound for measuring along the Gamma-M direction
+disp(['Measured lattice constant of GrIr(111) = ' num2str(a) ' A'])
+% Assuming similar uncertainties to the Ir(111) data we have approximatly +/-0.03A
--- figure_DiffractionIr.m
+++ figure_DiffractionIr.m
@@ -1,11 +1,11 @@
 
-clear all; clf
-fileList = {'as010376.mat','as010377.mat'}
+clear all; %clf
+fileList = {'as010376.mat','as010377.mat'};
 names = {'Ir(111)','H_2O Ir(111)'};
 colourList = 'rbgcmk';
-figure=figure('units','centimeters','position',[4,2,16,14],'color','white','DefaultLineLineWidth',2,'DefaultTextFontSize',18);
+figure('units','centimeters','position',[4,2,16,14],'color','white','DefaultLineLineWidth',2,'DefaultTextFontSize',18);
 
-addpath Diffraction
+addpath('Diffraction', 'Calibration')
 
 for i=1:length(fileList)
     load(fileList{i})
@@ -25,4 +25,62 @@ legend
 grid
 hold off
 
+
+%% Plot with Delta K rather than angle
+
+% Calibration of the beam 08/12/2021 12:19
+load('beam_20211208_1219.mat')
+beam.E0
+% Calibration of the beam 15/12/2021 16:08
+load('beam_20211215_1608.mat')
+beam.E0
+% The 2 diffraction scans being plotted in this script were taken 
+% 9/12/2021, 13:08 & 13:45
+% Use the calibration from 08/12/2021
+load('beam_20211208_1219.mat')
+E0 = beam.E0; % meV
+
+E0 = E0*1.603e-19/1e3; % J
+h = 6.626e-34; % Planck constant, Js
+m = 3.0160293*1.66054e-27; % Helium 3 rest mass, kg
+k = (2*pi*sqrt(2*E0*m)/h); % m^-1, incident wavector
+k = k/1e10; % A^-1
+disp(['k = ' num2str(k) ' A^-1'])
+
+load(fileList{1})
+Ir_gamma = meas.scan.angles-meas.ga;
+Ir_counts = meas.scan.counts;
+Ir_DK = -2*k*cosd(22.2)*sind(Ir_gamma);
+
+load(fileList{2})
+IrH20_gamma = meas.scan.angles-meas.ga;
+IrH20_counts = meas.scan.counts;
+IrH20_DK = -2*k*cosd(22.2)*sind(IrH20_gamma);
+
+figure('units','centimeters','position',[4,2,16,14],'color','white','DefaultLineLineWidth',2,'DefaultTextFontSize',18);
+semilogy(Ir_DK, Ir_counts, colourList(1), 'displayname', names{1})
+hold on
+semilogy(IrH20_DK, IrH20_counts, colourList(2), 'displayname', names{2})
+hold off
+xlabel('\Delta K (A^{-1})')
+ylabel('Scattered signal (arb units)')
+xlim([min(IrH20_DK), 0])
+legend
+set(gca,'YScale','log','LineWidth',1.5,'FontSize',18); set(gca,'ticklength',1.5*get(gca,'ticklength'))
+grid on
+
+% Find the diffraction peak to measure the lattice constant
+ind = Ir_DK < -2.5 & Ir_DK > -2.9;
+y = flip(Ir_counts(ind));
+x = flip(Ir_DK(ind));
+pks = findpeaks(y, x);
+G = abs(x(y == pks));
+a = (2*pi/G)/cosd(30); % Accound for measuring along the Gamma-M direction
+disp(['Measured lattice constant of Ir(111) = ' num2str(a) ' A'])
+% Note spacing of data points is ~0.02 A^(-1), giving ~0.02 A
+% uncertainty in the lattice constant. The beam energy also drifts slightly
+% 0.02meV over a week, so this is a significantly smaller uncertainty.
+% I think we can quote to 3 s.f. FWHM of the diffraction peak is ~0.07A^-1
+% which would give a standard deviation of the peak (assuming Gaussian) of
+% ~0.03A^-1. Translating to ~0.03A in the lattice constant.
 
